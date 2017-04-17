b0e4dbc2df1b4ae5d6b0df2b73a663c3c8a7a943
Hesham El-Sawaf <heshamlsawaf@gmail.com>
2017-04-17 04:47:04 +0200

    Added a simple material design.

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,69 @@
 package gui;
 
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.DoubleValidator;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Sample Skeleton for 'sample.fxml' Controller Class
+ */
+
 public class Controller {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="canvas"
+    private Canvas canvas; // Value injected by FXMLLoader
+
+    @FXML // fx:id="gainTF"
+    private JFXTextField gainTF; // Value injected by FXMLLoader
+
+    @FXML // fx:id="logger"
+    private JFXTextArea logger; // Value injected by FXMLLoader
+
+    @FXML
+    void addEdge(MouseEvent event) {
+
+    }
+
+    @FXML
+    void addNode(MouseEvent event) {
+
+    }
+
+    @FXML
+    void getResult(MouseEvent event) {
+
+    }
+
+    @FXML
+        // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert canvas != null;
+        assert gainTF != null;
+        assert logger != null;
+        DoubleValidator dv = new DoubleValidator();
+        this.gainTF.getValidators().add(dv);
+        dv.setMessage("Double value!");
+        this.gainTF.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (!newValue)
+                    gainTF.validate();
+            }
+        });
+    }
 }
+
